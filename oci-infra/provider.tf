005f1c3b6711751a492d01d140b28e339a1a0fe8
Nuno Moura <moura.abt@gmail.com>
2023-01-31 16:24:38 +0000

    first commit

---
+++ oci-infra/provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaaiwzu6wsexcjju362vgrbzjjelhjzgonvhbei5ijsqhw3eohv3pq"
+  user_ocid = "ocid1.user.oc1..aaaaaaaagzzqmwrvpkw5d2ivg5qsternleehwtn5i4y5aqsniad3xchppxda" 
+  private_key_path = "/Users/nmoura/OCI/Telma/oci_api_key.pem"
+  fingerprint = "7c:cb:81:79:a2:45:56:36:13:4e:34:00:c6:f3:df:22"
+  region = "eu-frankfurt-1"
+}
